ff62466fc5b541d0595d57f7d47bdf042ddb06e0
siva <sivaram0434@gmail.com>
2025-05-23 15:11:30 +0530

    add the helm provider

--- cluster/provider.tf
+++ cluster/provider.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.98.0"
     }
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.0-pre2"
+    }
   }
   backend "s3" {}
 }
---
+++ modules/eks/helm.tf
@@ -0,0 +1,6 @@
+resource "null_resource" "kube-bootstrap" {
+  depends_on = [aws_eks_cluster.example]
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name ${aws_eks_cluster.example.name}" 
+  }
+}
