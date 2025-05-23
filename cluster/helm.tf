4c966979dea6a39517874945752f549345530831
siva <sivaram0434@gmail.com>
2025-05-23 16:08:09 +0530

    update the config

--- cluster/helm.tf
+++ cluster/helm.tf
@@ -2,9 +2,6 @@ resource "null_resource" "kube-config" {
   depends_on = [module.eks]
 
   provisioner "local-exec" {
-    command = <<EOF
-aws eks update-kubeconfig --name ${module.eks.id} --region us-east-1
-kubectl get pods -A
-EOF
+    command = "aws eks update-kubeconfig --name ${module.eks.id} --region us-east-1"
   }
 }
