242a05a24a09774b247991981b677e52c19f8fdd
siva <sivaram0434@gmail.com>
2025-05-27 13:48:43 +0530

    add the helm charts

--- cluster/helm.tf
+++ cluster/helm.tf
@@ -6,30 +6,30 @@ resource "null_resource" "kube-config" {
   }
 }
 
-# resource "helm_release" "aws_lb_controller" {
-#   depends_on = [module.eks, null_resource.kube-config]
-#   name       = "aws-load-balancer-controller"
-#   namespace  = "kube-system"
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
+resource "helm_release" "aws_lb_controller" {
+  depends_on = [module.eks, null_resource.kube-config]
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
   
-#   set {
-#     name  = "clusterName"
-#     value = module.eks.id
-#   }
+  set {
+    name  = "clusterName"
+    value = module.eks.id
+  }
 
-#   set {
-#     name  = "serviceAccount.create"
-#     value = "false"
-#   }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
 
-#   set {
-#     name  = "region"
-#     value = var.region
-#   }
+  set {
+    name  = "region"
+    value = var.region
+  }
 
-#   set {
-#     name  = "vpcId"
-#     value = module.eks_vpc.vpc_id
-#   }
-# }
+  set {
+    name  = "vpcId"
+    value = module.eks_vpc.vpc_id
+  }
+}
