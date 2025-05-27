f764f1f9b4b9f8c601a29c910e2bdf6b866d2af5
siva <sivaram0434@gmail.com>
2025-05-27 11:10:40 +0530

    update the config

--- cluster/provider.tf
+++ cluster/provider.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.98.0"
     }
     helm = {
       source = "hashicorp/helm"
-      version = "3.0.0-pre2"
     }
   }
   backend "s3" {}
